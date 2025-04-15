be12e203000d2ed5a6ac22d7ea1eb6fd51b199f0
kiennd25 <ndkien98@gmail.com>
2025-04-15 21:31:28 +0700

    jdbc get data

--- core/core/src/buoi15/Main.java
+++ core/core/src/buoi15/Main.java
@@ -3,6 +3,7 @@ package buoi15;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -63,6 +64,25 @@ public class Main {
 
             System.out.println("B5: Hiển thị ra dữ liệu vua đọc được từ database ");
             productionsResult.forEach(System.out::println);
+            List<Integer> idProduction = productionsResult.stream().map(Production::getId).collect(Collectors.toList());
+            System.out.println("lấy thông tin detail");
+            String sqlDetail = "select production_detail.production_id,color.name,size.name,image.path from production_detail\n" +
+                    "inner join color on production_detail.color_id = color.id\n" +
+                    "inner join size on production_detail.size_id = size.id\n" +
+                    "inner join image on production_detail.image_id = image.id\n" +
+                    "where production_id in (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlDetail);
+            System.out.println("Set value to where condition with index ?,?");
+            for (int i = 0; i < idProduction.size(); i++) {
+                preparedStatement.setInt(i + 1, idProduction.get(i));
+            }
+            ResultSet resultSetDetail = preparedStatement.executeQuery();
+            while (resultSetDetail.next()) {
+                System.out.println(resultSetDetail.getString("production_detail.production_id"));
+                System.out.println(resultSetDetail.getString("color.name"));
+                System.out.println(resultSetDetail.getString("size.name"));
+                System.out.println(resultSetDetail.getString("image.path"));
+            }
 
         } catch (SQLException e) {
             System.out.println("Connection error: " + e.getMessage());
