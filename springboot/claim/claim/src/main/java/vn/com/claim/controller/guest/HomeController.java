b638127e8f49e8739696561bb37145b571924959
kiennd <ndkien98>
2025-06-06 19:54:21 +0700

    UI

---
+++ springboot/claim/claim/src/main/java/vn/com/claim/controller/guest/HomeController.java
@@ -0,0 +1,13 @@
+package vn.com.claim.controller.guest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/home","/"})
+    public String home() {
+        return "guest/home";
+    }
+}
---
+++ springboot/claim/claim/src/main/java/vn/com/claim/controller/guest/LoginController.java
@@ -0,0 +1,13 @@
+package vn.com.claim.controller.guest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "guest/loginmetlife";
+    }
+}
